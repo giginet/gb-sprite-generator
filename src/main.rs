f3bf3f64cddfb4bcceafb3966e5c2e5a3d541e7b
giginet <giginet.net@gmail.com>
2019-09-04 01:30:51 +0900

    Support convertion for directories

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,41 @@ pub mod utils;
 pub mod chopper;
 use source_generator::SourceGenerator;
 use converter::Converter;
-use std::path;
 
+use std::fs;
+use std::path;
 use std::env;
 
 fn main() {
     let mut args = env::args();
     args.next();
 
-    let img_path = match args.next() {
+    let dir_path = match args.next() {
         None => {
-            eprintln!("Error");
+            eprintln!("Usage: cargo run {{directory_path}}");
             return;
         },
         Some(s) => s
     };
-    println!("Converting {}...", img_path);
 
-    let converter = Converter { };
 
-    let all_headers = converter.load(&img_path);
-    
-    let path = path::Path::new(&img_path);
-    let variable_name = path.file_stem().unwrap();
-    let generator = SourceGenerator { };
-    let header = generator.generate(all_headers, &variable_name.to_str().unwrap());
-    println!("{}", header);
+    let base_dir = path::Path::new(&dir_path);
+    let entries = fs::read_dir(&dir_path).unwrap();
+    let images = entries
+        .map({ |entry| entry.ok().unwrap().file_name().into_string().ok().unwrap() } )
+        .filter({ |path| path.ends_with(".png") });
+
+    for image_name in images {
+        println!("Converting {}...", image_name);
+
+        let converter = Converter { };
+        let img_path = base_dir.join(image_name);
+        let path_string = img_path.to_str().unwrap();
+        let all_headers = converter.load(&path_string.to_string());
+
+        let variable_name = img_path.file_stem().unwrap();
+        let generator = SourceGenerator { };
+        let header = generator.generate(all_headers, &variable_name.to_str().unwrap());
+        println!("{}", header);
+    }
 }
